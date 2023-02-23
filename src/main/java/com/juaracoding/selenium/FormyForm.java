7db2033bf5ba893f4a41c09058ea65d1c910b027
“reynaldo <reynaldopaulini@yahoo.com>
2023-02-23 22:09:10 +0700

    First Commit

---
+++ src/main/java/com/juaracoding/selenium/FormyForm.java
@@ -0,0 +1,61 @@
+package com.juaracoding.selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FormyForm {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver","C:\\selenium\\chromedriver_win32\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://formy-project.herokuapp.com/form");
+        driver.manage().window().maximize();
+        System.out.println("Maximize Browser");
+
+        //isi nama depan
+        WebElement firstName = driver.findElement(By.id("first-name"));
+        firstName.sendKeys("Reynaldo");
+        System.out.println("print nama pertama");
+
+        //isi nama belakang
+        WebElement lastName = driver.findElement(By.id("last-name"));
+        lastName.sendKeys("Wonges");
+        System.out.println("print nama belakang");
+
+        //isi job title
+        WebElement jobTitle = driver.findElement(By.id("job-title"));
+        jobTitle.sendKeys("QA Engineer");
+        System.out.println("print job title");
+
+        //pilih level pendidikan
+        WebElement educationLevel = driver.findElement(By.id("radio-button-3"));
+        educationLevel.click();
+        System.out.println("eduction button clicked ");
+
+        //pilih pengalaman kerja
+        WebElement jobExperience = driver.findElement(By.id("checkbox-1"));
+        jobExperience.click();
+        System.out.println("job experience clicked");
+
+        //isi tanggal mulai kerja
+        WebElement startDate = driver.findElement(By.id("datepicker"));
+        startDate.sendKeys("02/23/2023");
+        System.out.println("print tanggal mulai kerja");
+
+        //klik tombol submit
+        WebElement submitButton = driver.findElement(By.xpath("//a[@class='btn btn-lg btn-primary']"));
+        submitButton.click();
+        System.out.println("submit button clicked");
+
+        System.out.println("Delay 3 Seconds");
+        try{
+            Thread.sleep(3000);
+        }catch (InterruptedException e){
+            throw new RuntimeException(e);
+        }
+        driver.quit();
+        System.out.println("Quit Browser");
+
+    }
+}
